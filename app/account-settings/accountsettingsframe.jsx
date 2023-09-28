74f0d74db5a63f446a9ae79be817785b14b65969
NickKhunapoj <Nickkipad@gmail.com>
2023-09-28 21:52:33 +0700

    edit text box colour in acc settings

--- app/account-settings/accountsettingsframe.jsx
+++ app/account-settings/accountsettingsframe.jsx
@@ -80,7 +80,7 @@ export const AccountSettingsFrame = () => {
                     <input
                         type="text"
                         placeholder="s65010126XXXXX@email.kmutnb.ac.th"
-                        className="w-full px-4 py-2 rounded-lg bg-[#21253180] backdrop-blur-[50px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(50px)_brightness(100%)] dark:text-gray-100  hover:bg-[#41465680] transition-all duration-300 ease-in-out"
+                        className="w-full px-4 py-2 rounded-lg bg-[#40404099] backdrop-blur-[50px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(50px)_brightness(100%)] dark:text-gray-100  hover:bg-[#41465680] transition-all duration-300 ease-in-out"
                         value={email}
                         onChange={handleEmailChange}
                         style={{ color: "white" }} // Set text color to white
@@ -91,7 +91,7 @@ export const AccountSettingsFrame = () => {
                     <input
                         type="text"
                         placeholder="KHUNAPOJ SUTTENON"
-                        className="w-full px-4 py-2 rounded-lg bg-[#21253180] backdrop-blur-[50px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(50px)_brightness(100%)] dark:text-gray-100  hover:bg-[#41465680] transition-all duration-300 ease-in-out"
+                        className="w-full px-4 py-2 rounded-lg bg-[#40404099] backdrop-blur-[50px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(50px)_brightness(100%)] dark:text-gray-100  hover:bg-[#41465680] transition-all duration-300 ease-in-out"
                         value={dpname}
                         onChange={handleDpnameChange}
                         style={{ color: "white" }} // Set text color to white
@@ -102,7 +102,7 @@ export const AccountSettingsFrame = () => {
                     <input
                         type="text"
                         placeholder="KHUNAPOJ"
-                        className="w-full px-4 py-2 rounded-lg bg-[#21253180] backdrop-blur-[50px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(50px)_brightness(100%)] dark:text-gray-100  hover:bg-[#41465680] transition-all duration-300 ease-in-out"
+                        className="w-full px-4 py-2 rounded-lg bg-[#40404099] backdrop-blur-[50px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(50px)_brightness(100%)] dark:text-gray-100  hover:bg-[#41465680] transition-all duration-300 ease-in-out"
                         value={username}
                         onChange={handleUsernameChange}
                         style={{ color: "white" }} // Set text color to white
